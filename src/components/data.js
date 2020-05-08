77b94e272126ba106be563d5720267ef78186d5f
Vichearith Meas <44794938+vichym@users.noreply.github.com>
2020-05-08 00:48:23 -0400

    store data in a file

---
+++ src/components/data.js
@@ -0,0 +1,38 @@
+export const Data = [
+  {
+    "Name": "Tres",
+    "Region": "Latin America",
+    "Country": "",
+    "FunFact": "A six strings guitar like instrument originate from Cuba",
+    "Group": ""
+  },
+  {
+    "Name": "Clave",
+    "Region": "Latin America",
+    "Country": "",
+    "FunFact": "A pair of short wooden dowels originated from sub-Saharan African and currently heard in Cuban music",
+    "Group": ""
+  },
+  {
+    "Name": "Timbales and Cowbell",
+    "Region": "Latin America",
+    "Country": "",
+    "FunFact": "",
+    "Group": ""
+  },
+  {
+    "Name": "Congas",
+    "Region": "Latin America",
+    "Country": "",
+    "FunFact": "",
+    "Group": ""
+  },
+  {
+    "Name": "Bajo sexto",
+    "Region": "Latin America",
+    "Country": "",
+    "FunFact": "12 strings and play bass rhythm and melodic counterpart to accordion",
+    "Group": ""
+  },
+
+]
