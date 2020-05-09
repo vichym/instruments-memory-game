afc8624c4c7b2f52912302f671558d1c9d5e2a1d
Vichearith Meas <44794938+vichym@users.noreply.github.com>
2020-05-09 02:54:45 -0400

    add data

--- src/components/data.js
+++ src/components/data.js
@@ -1,38 +1,171 @@
 export const Data = [
   {
-    "Name": "Tres",
-    "Region": "Latin America",
-    "Country": "",
-    "FunFact": "A six strings guitar like instrument originate from Cuba",
-    "Group": ""
+    name: "Tres",
+    region: "Latin America",
+    country: "",
+    funFact: "A six strings guitar like instrument originate from Cuba",
+    group: "Chordophone",
+    img: "Tres",
   },
   {
-    "Name": "Clave",
-    "Region": "Latin America",
-    "Country": "",
-    "FunFact": "A pair of short wooden dowels originated from sub-Saharan African and currently heard in Cuban music",
-    "Group": ""
+    name: "Clave",
+    region: "Latin America",
+    country: "",
+    funFact:
+      "A pair of short wooden dowels originated from sub-Saharan African and currently heard in Cuban music",
+    group: "Idiophone",
+    img: "Clave",
   },
   {
-    "Name": "Timbales and Cowbell",
-    "Region": "Latin America",
-    "Country": "",
-    "FunFact": "",
-    "Group": ""
+    name: "Timbales and Cowbell",
+    region: "Latin America",
+    country: "Cuba",
+    funFact:
+      "The Afro-Cuban percussions usually played in Danz√≥n, Mambo, Cha-cha-cha, Pachanga, Descarga , Salsa. ",
+    group: "Membranophone",
+    img: "Timbales",
   },
   {
-    "Name": "Congas",
-    "Region": "Latin America",
-    "Country": "",
-    "FunFact": "",
-    "Group": ""
+    name: "Congas",
+    region: "Latin America",
+    country: "Cuba",
+    funFact:
+      "Also known tumbadora, a tall, narrow, single-headed drum from Cuba. These instument uually comes in pair. ",
+    group: "Membranophone",
+    img: "Congas",
   },
   {
-    "Name": "Bajo sexto",
-    "Region": "Latin America",
-    "Country": "",
-    "FunFact": "12 strings and play bass rhythm and melodic counterpart to accordion",
-    "Group": ""
+    name: "Bajo Sexto",
+    region: "Latin America",
+    country: "Mexico",
+    funFact:
+      "A 12 strings instrument shape like guitar and plays bass rhythm and melodic counterpart to accordion. ",
+    group: "Chordophone",
+    img: "BajoSexto",
   },
-
-]
+  {
+    name: "Bongo Drum",
+    region: "Latin America",
+    country: "Cuba",
+    funFact:
+      "An Afro-Cuban percussion instrument that comes as a pair of small open bottomed drums of different sizes usually kept between thigh of musicians",
+    group: "Membranophone",
+    img: "Bongo",
+  },
+  {
+    name: "Guiro",
+    region: "Latin America",
+    country: "Latin America",
+    funFact:
+      "An instrument that produce sound by rubbing a stick or tines along the hollow gourd with parallel notches cut in one side. Usually found in traditional, folk music across Latin America. ",
+    group: "Idiophone",
+    img: "Guiro",
+  },
+  {
+    name: "Maracas",
+    region: "Latin America",
+    country: "",
+    funFact:
+      "Also known as tamaracas, an instrument that produce sound by shaking the stones or beans rattle against the hollow case. ",
+    group: "Idiophone",
+    img: "Maracas",
+  },
+  {
+    name: "Bata Drums",
+    region: "West Africa",
+    country: "",
+    funFact:
+      "A double-headed sacred drums used in Santeria religious rituals Cuba, West Africa, and Puerto Rico. Similar drums are seem in native culture land of Yoruba and Igbo, Nigeria. ",
+    group: "Membranophone",
+    img: "Bata",
+  },
+  {
+    name: "Balafon",
+    region: "West Africa",
+    country: "",
+    funFact:
+      "A type of struck idiophone created during the Mali Empire in the 12th century CE. A modern version of the the Southern African and South American is called Marimba. ",
+    group: "Xylophone",
+    img: "Balafon",
+  },
+  {
+    name: "Mbira",
+    region: "West Africa",
+    country: "Zimbabwe",
+    funFact:
+      "A plucked instrument with a given nickname as thumb piano. This metal-tined lamellophones first appeared in the Zambezi River valley around 1,300 years ago.",
+    group: "Lamellophone",
+    img: "Mbira",
+  },
+  {
+    name: "Kora",
+    region: "West Africa",
+    country: "",
+    funFact:
+      "An instrument that consists of twenty one strings that go over a bridge and connect to a long hardwood neck. The sound amplifier makes out of a large calabash cut in half and covered with cow skin and a vertical standing bridge.",
+    group: "Chordophone",
+    img: "Kora",
+  },
+  {
+    name: "Sitar",
+    region: "South Asian",
+    country: "India",
+    funFact:
+      " A plucked stringed instrument orignates from India that consists of six or seven main string and ten or more sympathetic strings. ",
+    group: "Chordophone",
+    img: "Sitar",
+  },
+  {
+    name: "Tabla",
+    region: "South Asian",
+    country: "India",
+    funFact:
+      "A precussion instrument consists of a pair of single-headed, barrel-shaped drums, used in traditional, classical, popular and folk music of North India",
+    group: "Membranophone",
+    img: "Tabla",
+  },
+  {
+    name: "Sarod",
+    region: "South Asian",
+    country: "India",
+    funFact:
+      "A seventeen to twenty-five-stringed  instrument known for a deep, weighty, introspective sound in constrast of that of Sitar.",
+    group: "Chordophone",
+    img: "Sarod",
+  },
+  {
+    name: "Tanpura",
+    region: "South Asian",
+    country: "India",
+    funFact:
+      "A plucked string instrument originating from India. This instrument does not play any melody but play continuous harmonic bourdon or drone. ",
+    group: "Chordophone",
+    img: "Tanpura",
+  },
+  {
+    name: "Mridangam",
+    region: "South Asian",
+    country: "India",
+    funFact:
+      "A double-headed drum popular in the South part of India while Tabla is popular in the North",
+    group: "Membranophone",
+    img: "Mridangam",
+  },
+  {
+    name: "Sarangi",
+    region: "South Asian",
+    country: "India",
+    funFact:
+      "A short-necked string instrument with a bow and a box-like shape body with three hollow chambers with 3 main string and 30 to 35 sympathic strings",
+    group: "Chordophone",
+    img: "Sarangi",
+  },
+];
+export const Region = ["Latin America", "West Africa", "South Asian"];
+export const Classification = [
+  "Chordophone",
+  "Idiophone",
+  "Membranophone",
+  "Lamellophone",
+  "Xylophone",
+];
