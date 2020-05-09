ae65d7af168057bc1d5a3bc50fab6dd08f413967
Vichearith Meas <44794938+vichym@users.noreply.github.com>
2020-05-09 02:55:08 -0400

    add game card design

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { memo } from "react";
 
-export default function GameCard({ handleClick, front, back, id, flipped }) {
+const  GameCard = ({
+  handleClick,
+  name,
+  front,
+  back,
+  id,
+  flipped,
+}) => {
   return (
     <div
       className={`flip-card ${flipped ? `flipped` : ""}`}
-      onClick={() => handleClick(id)}>
+      onClick={() => {
+        handleClick(id, name);
+        console.log("Click on card");
+      }}
+    >
       <div className="flip-card-inner">
-        <div className="flip-card-front">
-          {front}
-        </div>
-        <div className="flip-card-back">
-          {back}
-        </div>
+        <div className="flip-card-front">{front}</div>
+        <div className="flip-card-back" style={{background: '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)}}>{back}</div>
       </div>
     </div>
   );
 }
+export default memo(GameCard)
