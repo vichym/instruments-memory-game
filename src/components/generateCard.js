399badfe31b49577460b27c5fb40367c5c82931b
Vichearith Meas <44794938+vichym@users.noreply.github.com>
2020-05-09 02:56:02 -0400

    add code for randominze data

--- src/components/generateCard.js
+++ src/components/generateCard.js
@@ -1,39 +1,49 @@
+export default function generateCard(data) {
+  let id = 0;
+  const cards = [];
 
-export default function generateCard(data){
-    let id = 0
-    const cards = []
-    data.forEach(element => {
-        cards.push({
-            id :  id++, 
-            Name : element.Name, 
-            img:''
-        })
-        cards.push({
-            id : id++, 
-            Name: element.Name, 
-            Region :  element.Region,
-            FunFact: element.FunFact,
-            Group:element.Group
-        })
-    })
-    return shuffle(cards)
+  console.log("Generate Card!");
+  pickRandomFromArray(data, 5).forEach((element) => {
+    cards.push({
+      id: id++,
+      page: 1,
+      name: element.name,
+      img: element.img,
+    });
+    cards.push({
+      id: id++,
+      page: 2,
+      name: element.name,
+      region: element.region,
+      funFact: element.funFact,
+      group: element.group,
+      img: element.img,
+    });
+  });
+  return shuffle(cards);
+}
+function pickRandomFromArray(arr, n){
+  /* source: https://stackoverflow.com/questions/19269545/how-to-get-n-no-elements-randomly-from-an-array */
+  var shuffled = shuffle(arr);
+  return shuffled.slice(0,n);
 }
 
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-  
-    return array;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
+
+  return array;
+}
