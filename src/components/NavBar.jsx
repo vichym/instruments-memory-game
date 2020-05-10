ebe2b8d2520aafa24c8870dc0db8a53cf065cf98
Vichearith Meas <44794938+vichym@users.noreply.github.com>
2020-05-10 01:13:01 -0400

    remove material ui

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,33 +1,13 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
 
 export default function NavBar() {
-  const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Memory Game
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <nav class="navbar">
+        <h2 class="navbar-brand" href="#">
+          World Music 101 Memory Game Project
+        </h2>
+      </nav>
   );
 }
